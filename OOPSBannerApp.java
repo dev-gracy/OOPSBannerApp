13c4c64851b65e358459e89ff53e4bf2d198d312
dev-gracy <gg2666@srmist.edu.in>
2026-02-26 05:41:07 +0530

    UC1: print OOPS in console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,7 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-
-        System.out.println(" *****   *****   ******    ***** ");
-        System.out.println("*     * *     *  *     * *      *");
-        System.out.println("*     * *     *  *     * *       ");
-        System.out.println("*     * *     *  ******    ***** ");
-        System.out.println("*     * *     *  *              *");
-        System.out.println("*     * *     *  *        *     *");
-        System.out.println(" *****   *****   *         *****");
-
+        System.out.println("OOPS");
     }
+
 }
