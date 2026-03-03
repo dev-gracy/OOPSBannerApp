e3d34301e118d630928314c3cbb94e35f6bb9fa2
dev-gracy <gg2666@srmist.edu.in>
2026-03-03 10:59:30 +0530

    UC6: Refactor banner logic using static methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,52 @@
 public class OOPSBannerApp {
 
+    static String[] getOPattern() {
+        return new String[]{
+            " *****  ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            " *****  "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[]{
+            "******  ",
+            "*     * ",
+            "*     * ",
+            "******  ",
+            "*       ",
+            "*       ",
+            "*       "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[]{
+            " *****  ",
+            "*     * ",
+            "*       ",
+            " *****  ",
+            "      * ",
+            "*     * ",
+            " *****  "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join("", " *****  ", " ***** ", " ******  ", "  ***** "),
-            String.join("", "*     * ", "*     * ", "*     * ", "*      *"),
-            String.join("", "*     * ", "*     * ", "*     * ", "*"),
-            String.join("", "*     * ", "*     * ", "******   ", " *****     "),
-            String.join("", "*     * ", "*     * ", "* ", "             *     "),
-            String.join("", "*     * ", "*     * ", "* ", "       *     *     "),
-        String.join("", " *****  ", " ***** ", " * ", "        ***** "),
-        };
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
 
-        // Enhanced for loop to print banner
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O[i] + "  " +
+                               O[i] + "  " +
+                               P[i] + "  " +
+                               S[i]);
         }
     }
 }
